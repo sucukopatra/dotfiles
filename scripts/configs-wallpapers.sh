b84a443707a5b6f14a95e4f258c47fcd6db1f633
sucukopatra <endercortuk@proton.me>
2025-05-27 14:55:52 +0300

    i made it user-agnostic

--- scripts/configs-wallpapers.sh
+++ scripts/configs-wallpapers.sh
@@ -3,6 +3,9 @@
 # Set wallpaper
 wal -i ~/dotfiles/assets/wallpapers/pywallpaper.jpg -n
 
+# Change the "USER" placeholders in some config files
+find ~/dotfiles/assets/.config/wofi/ -type f -name "*.css" -exec sed -i "s|/home/USER|/home/$(whoami)|g" {} +; sed -i "s|USER|$(whoami)|g" ~/dotfiles/assets/autologin.conf; sed -i "s|/home/USER|/home/$(whoami)|g" ~/dotfiles/assets/.config/fastanime/config.ini
+
 # Copy files
 sudo cp -av ~/dotfiles/assets/wallpapers ~/
 sudo cp -a ~/dotfiles/assets/.config/. ~/.config/
